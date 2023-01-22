8de47ecedfa6210b73fcf0d37fe078dfc9c047dd
Harinisri2003 <108522448+Harinisri2003@users.noreply.github.com>
2023-01-22 14:25:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function generate()
+{
+    var div = document.getElementById('flex-box-gen');
+    let image = document.createElement('img');
+    image.src="https://th.bing.com/th/id/R.4f8aeb1c8e0facd34594872f1533068c?rik=hZqwQFq8WFNuUw&riu=http%3a%2f%2flikegif.com%2fwp-content%2fuploads%2f2013%2f03%2f01%2f100-funny-cat-animated-gifs-77.gif&ehk=YqlfoSMtKeUgD9MPyfXSS02BmHY2CwZHrRLkwTi3F3M%3d&risl=&pid=ImgRaw&r=0";
+    div.appendChild(image);
+}
